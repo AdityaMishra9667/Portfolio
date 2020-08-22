49e91970d9d82d0b17860d64cf36bf850d7253c8
AdityaMishra9667 <68849515+AdityaMishra9667@users.noreply.github.com>
2020-08-22 15:17:26 +0530

    Update projects.js

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -13,123 +13,146 @@ class projects extends Component {
     toggleCategories(){
         if(this.state.activeTab === 0){
             return(
-                <div className='projects-grid'>
-                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
-                        <CardTitle style={{color:'black', height:'176px', background:'url(https://logodix.com/logo/2058857.png) center'}}>
-                            Pygame Project #1
-                        </CardTitle>
-                        <CardText>
-                            Breadth First Search Visualizer
-                        </CardText>
-                        <CardActions border>
-                            <Button colored ripple href='https://github.com/AdityaMishra9667/Pygame-Experiments/blob/master/BFS.py'>GitHub</Button>
-                        </CardActions>
-                        <CardMenu style={{color:'#69bdd2'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                 <div className='projects-grid'>
+                    <Grid className="pygame">
+                        <Cell col={4}>
+                        <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                            <CardTitle style={{color:'black', height:'176px', background:'url(https://logodix.com/logo/2058857.png) center'}}>
+                                Pygame Project #1
+                            </CardTitle>
+                            <CardText>
+                                Breadth First Search Visualizer
+                            </CardText>
+                            <CardActions border>
+                                <Button colored ripple href='https://github.com/AdityaMishra9667/Pygame-Experiments/blob/master/BFS.py'>GitHub</Button>
+                            </CardActions>
+                            <CardMenu style={{color:'#69bdd2'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        </Cell>
 
-                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
-                        <CardTitle style={{color:'black', height:'176px', background:'url(https://logodix.com/logo/2058857.png) center'}}>
-                            Pygame Project #2
-                        </CardTitle>
-                        <CardText>
-                            Pendulum Physics
-                        </CardText>
-                        <CardActions border>
-                            <Button colored ripple href='https://github.com/AdityaMishra9667/Pygame-Experiments/blob/master/Pendulum.py'>GitHub</Button>
-                        </CardActions>
-                        <CardMenu style={{color:'#69bdd2'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                        <Cell col={4}>
+                        <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                            <CardTitle style={{color:'black', height:'176px', background:'url(https://logodix.com/logo/2058857.png) center'}}>
+                                Pygame Project #2
+                            </CardTitle>
+                            <CardText>
+                                Pendulum Physics
+                            </CardText>
+                            <CardActions border>
+                                <Button colored ripple href='https://github.com/AdityaMishra9667/Pygame-Experiments/blob/master/Pendulum.py'>GitHub</Button>
+                            </CardActions>
+                            <CardMenu style={{color:'#69bdd2'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        </Cell>
 
-                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
-                        <CardTitle style={{color:'black', height:'176px', background:'url(https://logodix.com/logo/2058857.png) center'}}>
-                            Pygame Project #3
-                        </CardTitle>
-                        <CardText>
-                            Cloth Physics
-                        </CardText>
-                        <CardActions border>
-                            <Button colored ripple href='https://github.com/AdityaMishra9667/Pygame-Experiments/blob/master/Cloth.py'>GitHub</Button>
-                        </CardActions>
-                        <CardMenu style={{color:'#69bdd2'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                        <Cell col={4}>
+                        <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                            <CardTitle style={{color:'black', height:'176px', background:'url(https://logodix.com/logo/2058857.png) center'}}>
+                                Pygame Project #3
+                            </CardTitle>
+                            <CardText>
+                                Cloth Physics
+                            </CardText>
+                            <CardActions border>
+                                <Button colored ripple href='https://github.com/AdityaMishra9667/Pygame-Experiments/blob/master/Cloth.py'>GitHub</Button>
+                            </CardActions>
+                            <CardMenu style={{color:'#69bdd2'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        </Cell>
 
-                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
-                        <CardTitle style={{color:'black', height:'176px', background:'url(https://logodix.com/logo/2058857.png) center'}}>
-                            Pygame Project #1
-                        </CardTitle>
-                        <CardText>
-                            Particle Physics
-                        </CardText>
-                        <CardActions border>
-                            <Button colored ripple href='https://github.com/AdityaMishra9667/Pygame-Experiments/blob/master/particles.py'>GitHub</Button>
-                        </CardActions>
-                        <CardMenu style={{color:'#69bdd2'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
-                </div>
+                        <Cell col={4}>
+                        <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                            <CardTitle style={{color:'black', height:'176px', background:'url(https://logodix.com/logo/2058857.png) center'}}>
+                                Pygame Project #1
+                            </CardTitle>
+                            <CardText>
+                                Particle Physics
+                            </CardText>
+                            <CardActions border>
+                                <Button colored ripple href='https://github.com/AdityaMishra9667/Pygame-Experiments/blob/master/particles.py'>GitHub</Button>
+                            </CardActions>
+                            <CardMenu style={{color:'#69bdd2'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        </Cell>             
+
+                        
+
+                        
+                    </Grid>
+                     
+                 </div>
             )
         }
 
         else if(this.state.activeTab === 1){
             return(
                 <div className='projects-grid'>
-                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
-                        <CardTitle style={{color:'white', height:'176px', background:'url(https://miro.medium.com/max/3000/1*m2gDBT_nc-iE7R4AM3sHBQ.jpeg) center'}}>
-                        Machine Learning Project #1
-                        </CardTitle>
-                        <CardText>
-                            Cats and Dogs Classification
-                        </CardText>
-                        <CardActions border>
-                            <Button colored ripple>
-                            <a href="https://github.com/AdityaMishra9667/Machine-Learning/tree/master/Cats%20and%20Dogs%20Dataset" target="popup">GitHub</a> 
-                            </Button>
-                        </CardActions>
-                        <CardMenu style={{color:'#69bdd2'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                  <Grid className="experiment">
+                      <Cell col={4}>
+                        <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                            <CardTitle style={{color:'white', height:'176px', background:'url(https://miro.medium.com/max/3000/1*m2gDBT_nc-iE7R4AM3sHBQ.jpeg) center'}}>
+                            Machine Learning Project #1
+                            </CardTitle>
+                            <CardText>
+                                Cats and Dogs Classification
+                            </CardText>
+                            <CardActions border>
+                                <Button colored ripple>
+                                <a href="https://github.com/AdityaMishra9667/Machine-Learning/tree/master/Cats%20and%20Dogs%20Dataset" target="popup">GitHub</a> 
+                                </Button>
+                            </CardActions>
+                            <CardMenu style={{color:'#69bdd2'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                      </Cell>
+                  
+                        <Cell col={4}>
+                            <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                                <CardTitle style={{color:'white', height:'176px', background:'url(https://miro.medium.com/max/3000/1*m2gDBT_nc-iE7R4AM3sHBQ.jpeg) center'}}>
+                                    Machine Learning Project #2
+                                </CardTitle>
+                                <CardText>
+                                    Face Recognition with OpenCV
+                                </CardText>
+                                <CardActions border>
+                                    <Button colored ripple>
+                                    <a href="https://github.com/AdityaMishra9667/Machine-Learning/tree/master/Image%20Detection" target="popup">GitHub</a>                                
+                                    </Button>
+                                </CardActions>
+                                <CardMenu style={{color:'#69bdd2'}}>
+                                    <IconButton name="share" />
+                                </CardMenu>
+                            </Card>
+                        </Cell>
 
-                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
-                        <CardTitle style={{color:'white', height:'176px', background:'url(https://miro.medium.com/max/3000/1*m2gDBT_nc-iE7R4AM3sHBQ.jpeg) center'}}>
-                            Machine Learning Project #2
-                        </CardTitle>
-                        <CardText>
-                            Face Recognition with OpenCV
-                        </CardText>
-                        <CardActions border>
-                            <Button colored ripple>
-                            <a href="https://github.com/AdityaMishra9667/Machine-Learning/tree/master/Image%20Detection" target="popup">GitHub</a>                                
-                            </Button>
-                        </CardActions>
-                        <CardMenu style={{color:'#69bdd2'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
-                
-                     <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
-                        <CardTitle style={{color:'white', height:'176px', background:'url(https://miro.medium.com/max/3000/1*m2gDBT_nc-iE7R4AM3sHBQ.jpeg) center'}}>
-                            Machine Learning Project #3
-                        </CardTitle>
-                        <CardText>
-                            AI plays Flappy Birds
-                        </CardText>
-                        <CardActions border>
-                            <Button colored ripple>
-                            <a href="https://github.com/AdityaMishra9667/Machine-Learning/tree/master/FlappyBirdAI" target="popup">GitHub</a>                                
-                            </Button>
-                        </CardActions>
-                        <CardMenu style={{color:'#69bdd2'}}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
+                        <Cell col={4}>
+                            <Card shadow={5} style={{minWidth:'450', margin:'auto'}}>
+                                <CardTitle style={{color:'white', height:'176px', background:'url(https://miro.medium.com/max/3000/1*m2gDBT_nc-iE7R4AM3sHBQ.jpeg) center'}}>
+                                    Machine Learning Project #3
+                                </CardTitle>
+                                <CardText>
+                                    AI plays Flappy Birds
+                                </CardText>
+                                <CardActions border>
+                                    <Button colored ripple>
+                                    <a href="https://github.com/AdityaMishra9667/Machine-Learning/tree/master/FlappyBirdAI" target="popup">GitHub</a>                                
+                                    </Button>
+                                </CardActions>
+                                <CardMenu style={{color:'#69bdd2'}}>
+                                    <IconButton name="share" />
+                                </CardMenu>
+                            </Card>
+                        </Cell>
+                    </Grid>
                 </div>
             )
         }
@@ -144,12 +167,9 @@ class projects extends Component {
                     <Tab>Pygame</Tab>
                     <Tab>Machine Learning</Tab>
                 </Tabs>
-
-                    <Grid>
-                        <Cell col={12}>
-                            <div className="content">{this.toggleCategories()}</div>
-                        </Cell>
-                    </Grid>                    
+                <div className="hello">
+                {this.toggleCategories()}
+                    </div>
             </div>
         )
     }
